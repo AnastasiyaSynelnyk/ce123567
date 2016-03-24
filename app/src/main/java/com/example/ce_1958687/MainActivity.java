1c14cb84b0ea25bc7dc2ae03e4381a3e86a06693
Anastasiya <inth333@gmail.com>
2016-03-24 23:11:10 +0200

    add comm new

--- app/src/main/java/com/example/ce_1958687/MainActivity.java
+++ app/src/main/java/com/example/ce_1958687/MainActivity.java
@@ -1,24 +1,24 @@
 package com.example.ce_1958687;
 
-import android.app.ActionBar;
-import android.content.DialogInterface;
-import android.content.res.Resources;
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.Toolbar;
-import android.view.Gravity;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.LinearLayout;
-import android.widget.TextView;
-import android.widget.Toast;
+        import android.app.ActionBar;
+        import android.content.DialogInterface;
+        import android.content.res.Resources;
+        import android.os.Bundle;
+        import android.support.design.widget.FloatingActionButton;
+        import android.support.design.widget.Snackbar;
+        import android.support.v7.app.AppCompatActivity;
+        import android.support.v7.widget.LinearLayoutManager;
+        import android.support.v7.widget.RecyclerView;
+        import android.support.v7.widget.Toolbar;
+        import android.view.Gravity;
+        import android.view.View;
+        import android.view.Menu;
+        import android.view.MenuItem;
+        import android.widget.LinearLayout;
+        import android.widget.TextView;
+        import android.widget.Toast;
 
-import org.w3c.dom.Text;
+        import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
